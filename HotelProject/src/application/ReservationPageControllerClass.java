66438a760f48f4b20b7d2d1214180dd8c17abde7
nwahba02 <nathanwahba@localhost>
2023-10-16 16:01:45 -0700

    changes

--- HotelProject/src/application/ReservationPageControllerClass.java
+++ HotelProject/src/application/ReservationPageControllerClass.java
@@ -135,7 +135,8 @@ public class ReservationPageControllerClass {
 		
 		Reserve res = new Reserve();
 		Room rom  = new Room ("101","single","open","115");
-		Customer cus = new Customer(reservationFirstName,reservationLastName,reservationEmail,reservationPhoneNumber);
+		Customer cus = new Customer(reservationFirstName,reservationLastName,reservationEmail,reservationPhoneNumber,reservationCardFirstName,reservationCardLastName,
+                reservationCardPaymentNumber,reservationCardExpMonth,reservationCardZipcode,reservationCardCountry);
 		res.reserveRoom(rom, cus);
 		//Temporarily returns the user to the main screen on success until we make a reservation review scene
 		root = FXMLLoader.load(getClass().getResource("mainPage.FXML"));
