623d9704894731e3daa996fe048373188335414e
nwahba02 <nathanwahba@localhost>
2023-11-15 15:35:21 -0800

    fixed change reservation

--- HotelProject/src/application/EditPageControllerClass.java
+++ HotelProject/src/application/EditPageControllerClass.java
@@ -26,15 +26,15 @@ public class EditPageControllerClass implements Initializable {
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
-	
+
 	@FXML
-	private TextField editFirstName, editLastName, editEmail, editPhone, editCCfirstName, editCClastName, editCCnumber, editCCexperation,
-			editCCcountry, editCCzip, editCCsecurity, editReservationNumber;
+	private TextField editFirstName, editLastName, editEmail, editPhone, editCCfirstName, editCClastName, editCCnumber,
+			editCCexperationMonth,editCCexperationYear, editCCcountry, editCCzip, editCCsecurity, editReservationNumber;
 	@FXML
 	private DatePicker reservationPageCheckIn, reservationPageCheckOut;
 	@FXML
 	private RadioButton reservationPageSingle, reservationPageDouble, reservationPageKing, reservationPageSuite;
-	
+
 	String reservationFirstName;
 	String reservationLastName;
 	String reservationEmail;
@@ -54,42 +54,40 @@ public class EditPageControllerClass implements Initializable {
 
 	@Override
 	public void initialize(URL url, ResourceBundle resourcebundle) {
-		
-		Reserve res = new Reserve();
-		 
-		ID = review.getID();
 
-		if (res.checkID(ID) == true) {		
+		Reserve res = new Reserve();
+
+		ID = review.getID();
+		if (res.checkID(ID) == true) {
+			//reservationNumber needs to be switched to a label instead of textField
 			//editReservationNumber.setText(ID);
 			editFirstName.setText(res.findCustomerID(ID).firstName);
 			editLastName.setText(res.findCustomerID(ID).lastName);
 			editEmail.setText(res.findCustomerID(ID).email);
 			editPhone.setText(res.findCustomerID(ID).phoneNumber);
-			
-			// I need to edit this to fit a radio button 
-			//reviewRoomType.setText(res.findCustomerID(ID).phoneNumber);
+
+			// I need to edit this to fit a radio button
+			// reviewRoomType.setText(res.findCustomerID(ID).phoneNumber);
 			// reviewRoomNumber.setText(res.findCustomerID(ID).roomNumber);
 			editCCfirstName.setText(res.findCustomerID(ID).firstName);
 			editCClastName.setText(res.findCustomerID(ID).lastName);
-			// I need to edit this to fit a datpicker object 
-			//reviewCheckIn.setText(res.findCustomerID(ID).checkIn);
 			// I need to edit this to fit a datpicker object
-			//reviewCheckOut.setText(res.findCustomerID(ID).checkOut);
+			// reviewCheckIn.setText(res.findCustomerID(ID).checkIn);
+			// I need to edit this to fit a datpicker object
+			// reviewCheckOut.setText(res.findCustomerID(ID).checkOut);
 			editCCcountry.setText(res.findCustomerID(ID).country);
-		
 
 			editCCnumber.setText(res.findCustomerID(ID).cardNumber);
 			editCCzip.setText(res.findCustomerID(ID).zipCode);
-			//editCCexperation.setText(res.findCustomerID(ID).expDate);
+			// editCCexperation.setText(res.findCustomerID(ID).expDate);
 		} else {
 			ReadWriteExcel obj = new ReadWriteExcel();
-	        int CustomerCount = 1;
-	        while (obj.isNull(CustomerCount, 1,0) == false) {
-	            CustomerCount++;
-	        }
-	        CustomerCount--;
-	        
-	        
+			int CustomerCount = 1;
+			while (obj.isNull(CustomerCount, 1, 0) == false) {
+				CustomerCount++;
+			}
+			CustomerCount--;
+
 			editFirstName.setText(obj.ReadExcel("Customers", CustomerCount, 1));
 			editLastName.setText(obj.ReadExcel("Customers", CustomerCount, 2));
 			editEmail.setText(obj.ReadExcel("Customers", CustomerCount, 3));
@@ -97,26 +95,69 @@ public class EditPageControllerClass implements Initializable {
 			editCCfirstName.setText(obj.ReadExcel("Customers", CustomerCount, 5));
 			editCClastName.setText(obj.ReadExcel("Customers", CustomerCount, 6));
 			editCCnumber.setText(obj.ReadExcel("Customers", CustomerCount, 7));
-			//editCCexperation.setText(obj.ReadExcel("Customers", CustomerCount, 8));
+			// editCCexperation.setText(obj.ReadExcel("Customers", CustomerCount, 8));
 			editCCcountry.setText(obj.ReadExcel("Customers", CustomerCount, 9));
 			editCCzip.setText(obj.ReadExcel("Customers", CustomerCount, 10));
-			//editReservationNumber.setText(obj.ReadExcel("Customers", CustomerCount, 11));
-			//reviewRoomNumber.setText(obj.ReadExcel("Customers", CustomerCount, 12));
-			//reviewPrice.
-			//reviewCheckIn.setText(obj.ReadExcel("Customers", CustomerCount, 14));
-			//reviewCheckOut.setText(obj.ReadExcel("Customers", CustomerCount, 15));
+			// editReservationNumber.setText(obj.ReadExcel("Customers", CustomerCount, 11));
+			// reviewRoomNumber.setText(obj.ReadExcel("Customers", CustomerCount, 12));
+			// reviewPrice.
+			// reviewCheckIn.setText(obj.ReadExcel("Customers", CustomerCount, 14));
+			// reviewCheckOut.setText(obj.ReadExcel("Customers", CustomerCount, 15));
 		}
-		
+
 	}
 
-	
+	public void roomType() {
+		if (reservationPageSingle.isSelected()) {
+			reservationRoomType = "Single";
+		} else if (reservationPageDouble.isSelected()) {
+			reservationRoomType = "Double";
+		} else if (reservationPageKing.isSelected()) {
+			reservationRoomType = "King";
+		} else if (reservationPageSuite.isSelected()) {
+			reservationRoomType = "Suite";
+		}
+	}
+	public void setNewChanges() {
+		roomType();
+		
+		reservationFirstName = editFirstName.getText();
+		reservationLastName = editLastName.getText();
+		reservationEmail = editEmail.getText();
+		reservationPhoneNumber = editPhone.getText();
+		reservationCardFirstName = editCCfirstName.getText();
+		reservationCardLastName = editCClastName.getText();
+		reservationCardPaymentNumber = editCCnumber.getText();
+		//reservationCardExpMonth = editCCexperation.getText();
+		//reservationCardExpYear = editCCexperation.getText();
+		reservationCardCountry = editCCcountry.getText();
+		reservationCardZipcode = editCCzip.getText();
+		
+		//reservationRoomType = editRoomType.getText();
+		
+		//change to get a date value
+		//checkOut = reservationPageCheckOut.getText();
+		//checkIn = reservationPageCheckIn.getText();
+		
+		expCombined = reservationCardExpMonth + "/" + reservationCardExpYear;
+	}
+
 	@FXML
 	public void confirmChanges(ActionEvent event) throws IOException {
+		setNewChanges();
 		Reserve reserve = new Reserve();
-		//reserve.changeReservation(ID, Customer);
+		Customer customer = new Customer(reservationFirstName, reservationLastName, reservationEmail, reservationPhoneNumber, reservationCardFirstName, 
+				reservationCardLastName, reservationCardPaymentNumber,expCombined, reservationCardZipcode, reservationCardCountry);
+		customer.ID = ID;
+		reserve.changeReservation(ID, customer);
+		// returns back to main page
+		root = FXMLLoader.load(getClass().getResource("mainPage.FXML"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
 	}
-	
-	
+
 	@FXML
 	public void switchToMainScene(ActionEvent event) throws IOException {
 		root = FXMLLoader.load(getClass().getResource("mainPage.FXML"));
