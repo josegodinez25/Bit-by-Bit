23d951b727e21324064e986fb34a15e03c55f4e5
nwahba02 <nathanwahba@localhost>
2023-11-20 15:40:17 -0800

    updating branch

--- HotelProject/src/application/Main.java
+++ HotelProject/src/application/Main.java
@@ -6,7 +6,6 @@ import javafx.application.Application;
 
 public class Main {
 
-	
 
 	public static void main(String[] args) throws IOException {
 		Application.launch(MainPageControllerClass.class, args);
