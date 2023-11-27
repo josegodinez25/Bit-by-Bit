2071a91f9a1c16ee1f5c09ed8533a65dbc026c11
ericr <ericr@DESKTOP-4SGLLH7.csun.edu>
2023-11-27 15:34:54 -0800

    test library

---
+++ HotelProject/src/application/ReserveTest.java
@@ -0,0 +1,59 @@
+package application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ReserveTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	final void testIsBooked() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testRandomGenerator() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testReserveRoom() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testSendEmail() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testFindCustomerID() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testCheckID() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testChangeReservation() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testClearCustomerInfo() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
